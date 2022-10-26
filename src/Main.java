885da162fd231af2bf238f5c6d481b1ee781c01a
Mergen <exciton08@mail.ru>
2022-10-26 17:42:26 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 17;
-        for (i = 0; i <= 17; i++) {
-            System.out.println(i);
-        }
+        System.out.println("Hello World");
     }
 }
